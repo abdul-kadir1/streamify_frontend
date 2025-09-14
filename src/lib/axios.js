8f573bac071f1c99fd62806a875ec48d7fdcf267
Abdul Kadir <abdulkadir80949@gmail.com>
2025-09-14 16:54:38 +0530

    Updated

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -17,7 +17,7 @@ const BASE_URL = import.meta.env.VITE_API_BASE_URL + "/api";
 
 const axiosInstance = axios.create({
   baseURL: BASE_URL,
-  withCredentials: true, // send cookies with the request
+  withCredentials: true, // send cookies with the request 
 });
 
 export default axiosInstance;
